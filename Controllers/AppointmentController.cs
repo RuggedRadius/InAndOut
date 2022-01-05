6fe727338202ce5408bb1b9fb4f903678a905b97
Ben Royans <ben.royans@gmail.com>
2022-01-05 17:56:36 +0800

    Initial commit

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            string todaysDate = DateTime.Now.ToShortDateString();
+
+            return Ok("You have entered id = " + id + "\r\non\r\n" + todaysDate);
+        }
+    }
+}
